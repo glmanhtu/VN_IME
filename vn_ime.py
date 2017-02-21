da218161a88613673161ab944db04d7bd14fa159
xtuanta <88d52bdba0366127fffca9dfa93895@users.noreply.github.com>
2017-02-21 23:40:17 +0700

    Fix multiviews layout

--- vn_ime.py
+++ vn_ime.py
@@ -1,35 +1,24 @@
 from .bogo.core import get_vni_definition, process_sequence
 import sublime, sublime_plugin
 
-MOD = False
 STATUS = False
 TELEX = False
 
 class SaveOnModifiedListener(sublime_plugin.EventListener):
-  def on_modified(self, view):
-    global MOD
-
-    if not MOD:
-      view.run_command('startime')
-    MOD = False
+  def on_modified_async(self, view):
+    view.run_command('startime')
 
 class StartimeCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    global MOD
-
-    if not STATUS:
-      return False
-
-    current_position = self.view.sel()[0]
-    word_region = self.view.word(current_position)
+    if not STATUS: return False
+    curr_pos = self.view.sel()[0]
+    word_region = self.view.word(curr_pos)
     word = self.view.substr(word_region)
     final = self.process(word)
-
-    if not final:
-      return False
-
+    #
+    if not final: return False
+    self.view.end_edit(edit)
     self.view.run_command("runchange", {"string":final})
-    MOD = True
     return True
 
   def process(self, word):
@@ -37,7 +26,6 @@ class StartimeCommand(sublime_plugin.TextCommand):
       final_word = process_sequence(word)
     else:
       final_word = process_sequence(word, rules=get_vni_definition())
-
     if final_word != word:
       return final_word
     return False
@@ -46,14 +34,13 @@ class ControlimeCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     global STATUS
     global TELEX
-    global SKIP_NON_VIETNAMESE
-
+    #
     settings = sublime.load_settings("Preferences.sublime-settings")
     if settings.get("telex"):
       TELEX = True
     else:
       TELEX = False
-
+    #
     if STATUS:
       STATUS = False
       self.view.set_status('VN IME'," VN IME: OFF")
@@ -63,33 +50,18 @@ class ControlimeCommand(sublime_plugin.TextCommand):
 
 class RunchangeCommand(sublime_plugin.TextCommand):
   def run(self, edit, string):
-    current_position = self.view.sel()[0]
-    region = self.view.word(current_position)
+    curr_pos = self.view.sel()[0]
+    region = self.view.word(curr_pos)
     self.view.replace(edit, region, string)
 
 class FuncundoCommand(sublime_plugin.WindowCommand):
   def run(self):
-    global MOD
-    
-    tmp_MOD = MOD
-    MOD = True
     self.window.run_command("undo")
-    MOD = tmp_MOD
 
 class FuncpasteCommand(sublime_plugin.WindowCommand):
   def run(self):
-    global MOD
-    
-    tmp_MOD = MOD
-    MOD = True
     self.window.run_command("paste")
-    MOD = tmp_MOD
 
 class FuncredoCommand(sublime_plugin.WindowCommand):
   def run(self):
-    global MOD
-
-    tmp_MOD = MOD
-    MOD = True
     self.window.run_command("redo")
-    MOD = tmp_MOD
