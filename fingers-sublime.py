bbc3b16db1820189e8cbc833dffaf924d2126a0e
Tien Dung <dungtn@gmail.com>
2022-09-08 06:39:30 +0700

    Chỉ convert sang TV khi current position là ký tự

--- fingers-sublime.py
+++ fingers-sublime.py
@@ -29,13 +29,20 @@ class InsertCharCommand(sublime_plugin.TextCommand):
 
 class KeyPressedCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        # Bỏ qua nếu chế độ gõ Telex đang tắt
         if not TELEXIFY: return False
-        curr_cursor = self.view.sel()[0]
-        word_region = self.view.word(curr_cursor)
-        origin = self.view.substr(word_region)
 
-        last_char = origin[-1].lower()
-        if last_char not in "qwrsfjxd": return False
+        curr_cursor = self.view.sel()[0]
+        # Bỏ qua nếu có selected text
+        if curr_cursor.begin() != curr_cursor.end(): return False
+
+        word_region = self.view.word(curr_cursor)
+        curr_region = sublime.Region(word_region.begin(), curr_cursor.begin())
+        origin = self.view.substr(curr_region)
+
+        # Bỏ qua nếu ký tự cuối ko có tác dụng chuyển ngữ
+        if origin[-1].lower() not in "qwrsfjxd": return False
+
         final = process_sequence(origin)
         if final == origin: return False
         self.view.end_edit(edit)
@@ -45,9 +52,10 @@ class KeyPressedCommand(sublime_plugin.TextCommand):
 
 class ReplaceCurrentCommand(sublime_plugin.TextCommand):
     def run(self, edit, string):
-        curr_pos = self.view.sel()[0]
-        region = self.view.word(curr_pos)
-        self.view.replace(edit, region, string)
+        curr_cursor = self.view.sel()[0]
+        word_region = self.view.word(curr_cursor)
+        curr_region = sublime.Region(word_region.begin(), curr_cursor.begin())
+        self.view.replace(edit, curr_region, string)
 
 
 class ToggleTelexModeCommand(sublime_plugin.TextCommand):
